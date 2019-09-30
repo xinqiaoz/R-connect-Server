8c3bd1a325db7856f1c11595bb71d6123c22c1b4
Kerui Cao <keruicao@bu.edu>
2019-09-30 00:01:50 -0400

    Started HW3 of Modeling.

--- Modelling Homework/HW2/Debug_file.R
+++
@@ -1,5 +0,0 @@
-for(i in 1:length(names(heights))){
-  num_of_uniquevalue=length(unique(heights[,i]))
-  Na = sum(is.na(heights[,i]))
-  info = cbind(info,matrix(c(num_of_uniquevalue,Na),ncol = 1))
-}
