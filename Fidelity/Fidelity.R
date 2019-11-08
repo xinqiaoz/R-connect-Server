d904f50060b1ed9651ac402e6ba8ddfe006e9d6a
Kerui Cao <keruicao@bu.edu>
2019-11-08 13:51:43 -0500

    Finished Modeling HW6-7

--- Fidelity/Fidelity.R
+++ Fidelity/Fidelity.R
@@ -22,6 +22,49 @@ hist(Merrill.Lynch.Global.Bond$PX_LAST,breaks = 100)
 data = left_join(FCRDX,Merrill.Lynch.Global.Bond,by = "Date")
 data = drop_na(data)
 abline(coef = c(0,1))
+library(tidyverse)
+
+data= data %>% mutate(Adj.Close = (Adj.Close-min(Adj.Close))/(max(Adj.Close)-min(Adj.Close)))
+data= data %>% mutate(PX_LAST = (PX_LAST-min(PX_LAST))/(max(PX_LAST)-min(PX_LAST)))
+
+a = data$Adj.Close
+b = data$PX_LAST
+
+a.s = sample(a,size = 500,replace = T)
+b.s = sample(b,size = 500,replace = T)
+sum(a.s>b.s)/length(a.s)
+a.ss = sample(a,size = 500,replace = T)
+sum(a.s>a.ss)/length(a.s)
+
+###############################################################################
+
+n = 1000
+x.1 = runif(n,min = 10,max = 25)
+x.2 = rnorm(mean = 3,sd = 3,n = n)
+x.3 = rpois(n = n,lambda = 5)
+y1 = 5 + 4.4*x.1[1:(n/2)] - 3.5*x.2[1:(n/2)] + 3.3*x.3[1:(n/2)] + rnorm(n/2)
+y2 = 2 + 8*x.1[(n/2+1):n] + 3.5*x.2[(n/2+1):n] + 2*x.3[(n/2+1):n] + rnorm(n/2)
+y = c(y1,y2)
+
+lm1 = lm(y1~x.1[1:(n/2)]+x.2[1:(n/2)]+x.3[1:(n/2)])
+coef(lm1)
+lm2 = lm(y2~x.1[(n/2+1):n]+x.2[(n/2+1):n]+x.3[(n/2+1):n])
+coef(lm2)
+lm3 = lm(y~x.1+x.2+x.3)
+summary(lm3)
+
+sc = sum(lm3$residuals^2)
+s1 = sum(lm1$residuals^2)
+s2 = sum(lm2$residuals^2)
+n1 = n/2
+n2 = n/2
+k = 3
+f.test = ((sc-(s1+s2))/k)/((s1+s2)/(n1+n2-2*k))
+pf(df1 = k,df2 = (n1+n2-2*k),q = f.test,lower.tail = F)
+
+###############################################################################
+
+
 library(ggplot2)
 ggplot()+geom_line(aes(y = quantile(data$Adj.Close,seq(0,1,0.01)),x = quantile(data$PX_LAST,seq(0,1,0.01))))+
   geom_abline(slope = 1,intercept = 0)
